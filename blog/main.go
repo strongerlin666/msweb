1418ba7cb4d54fee0c8d9eca4b14caf6fb1ebaaf
strongerlin666 <linjun_0510@163.com>
2022-09-09 07:52:58 +0800

    实现简单路由功能

--- blog/main.go
+++ blog/main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"github.com/strongerlin666/msweb/msgo"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, "%s 欢迎来到码神之路goweb教程", "mszlu.com")
+	//http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+	//	// TODO: error处理
+	//	fmt.Fprintf(writer, "%s 欢迎来到码神之路goweb教程", "mszlu.com")
+	//})
+	//err := http.ListenAndServe(":8111", nil)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	engine := msgo.New()
+	engine.Add("/hello", func(w http.ResponseWriter, r *http.Request) {
+		// TODO: error处理
+		fmt.Fprintf(w, "%s 欢迎来到码神之路goweb教程", "mszlu.com")
 	})
-	err := http.ListenAndServe(":8111", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	engine.Run()
 }
